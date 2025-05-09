779ed4464b76957346dba634ab382242c0df5065
Samuel Silva <samuelcassimirof@gmail.com>
2025-05-08 22:15:46 -0300

    new

--- index.js
+++
@@ -1,5 +0,0 @@
-function mostrarTodos(clicado) {
-    const personagens = document.querySelectorAll('.personagem');
-    personagem-selecionar.forEach(p => p.classList.add('mostrar'));
-    clicado.onclick = null;
- }
